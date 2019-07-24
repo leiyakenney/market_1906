90c0a1a6f5fc111eb980701341ea20bae0cad18f
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-24 10:10:11 -0600

    Set up vendor/test

---
+++ test/vendor_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_attributes
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+
+    expected = {}
+    assert_equal expected, @vendor.inventory
+  end
+
+end
