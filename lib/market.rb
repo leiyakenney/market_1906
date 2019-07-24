7f0c44fa37f343f2df34debd9735a24553fc3b33
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-24 11:11:55 -0600

    Complete iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -14,15 +14,32 @@ class Market
     @vendors.map { |vendor| vendor.name }
   end
 
-  def vendors_that_sell(food)
+  def vendors_that_sell(item)
     food_vendor = []
 
     @vendors.each do |vendor|
-      if vendor.inventory.keys.include?(food)
+      if vendor.inventory.keys.include?(item)
         food_vendor << vendor
       end
     end
     food_vendor
+  end
+
+  def sorted_item_list
+    items = []
+    @vendors.each { |vendor| items << vendor.inventory.keys}
+    items.flatten.uniq.sort
+  end
+
+  def total_inventory
+    tot_inventory = Hash.new(0)
+
+    @vendors.each do |vendor|
+      vendor.inventory.each do |name,amt|
+        tot_inventory[name] += amt
+      end
+    end
+    tot_inventory
   end
 
 end
